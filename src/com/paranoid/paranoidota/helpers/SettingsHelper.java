65ee15b83e86038d9c183d0a0e8f18796f2f075f
cr3pt <cr3pt@o2.pl>
2014-04-11 21:08:56 +0200

    dzieki Michał

--- src/com/paranoid/paranoidota/helpers/SettingsHelper.java
+++ src/com/paranoid/paranoidota/helpers/SettingsHelper.java
@@ -29,6 +29,7 @@ public class SettingsHelper {
     public static final int GAPPS_MINI = 1;
     public static final int GAPPS_STOCK = 2;
     public static final int GAPPS_MICRO = 3;
+	public static final int GAPPS_ESSENTIAL = 4;
 
     public static final String PROPERTY_CHECK_TIME = "checktime";
     public static final String PROPERTY_CHECK_GAPPS = "checkgapps";
