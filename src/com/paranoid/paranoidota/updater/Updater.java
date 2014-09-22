fe486a2273445967554831e89da05555ab903664
cr3pt <cr3pt@o2.pl>
2014-09-22 22:28:54 +0200

    Revert "Update Updater.java"
    This reverts commit 971a459c765a669915cb6bcc97401b21408af61f.

--- src/com/paranoid/paranoidota/updater/Updater.java
+++ src/com/paranoid/paranoidota/updater/Updater.java
@@ -19,12 +19,6 @@
 
 package com.paranoid.paranoidota.updater;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.content.Context;
 
@@ -34,9 +28,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.paranoid.paranoidota.R;
 import com.paranoid.paranoidota.Utils;
 import com.paranoid.paranoidota.Version;
 import com.paranoid.paranoidota.helpers.SettingsHelper;
+
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Updater implements Response.Listener<JSONObject>, Response.ErrorListener {
 
@@ -81,7 +82,7 @@ public abstract class Updater implements Response.Listener<JSONObject>, Response
 
     private Context mContext;
     private Server[] mServers;
-    private PackageInfo[] mLastUpdates;
+    private PackageInfo[] mLastUpdates = new PackageInfo[0];
     private List<UpdaterListener> mListeners = new ArrayList<UpdaterListener>();
     private RequestQueue mQueue;
     private SettingsHelper mSettingsHelper;
@@ -119,6 +120,9 @@ public abstract class Updater implements Response.Listener<JSONObject>, Response
     }
 
     public void setLastUpdates(PackageInfo[] infos) {
+        if (infos == null) {
+            infos = new PackageInfo[0];
+        }
         mLastUpdates = infos;
     }
 
@@ -172,11 +176,17 @@ public abstract class Updater implements Response.Listener<JSONObject>, Response
                 for (int i = 0; i < list.size(); i++) {
                     info = list.get(i);
                     String fileName = info.getFilename();
-                    if ((gappsType == SettingsHelper.GAPPS_MINI && !fileName.contains("-mini")) ||
-                            (gappsType == SettingsHelper.GAPPS_STOCK && !fileName.contains("-stock")) ||
-                            (gappsType == SettingsHelper.GAPPS_FULL && !fileName.contains("-full")) ||
-			    (gappsType == SettingsHelper.GAPPS_ESSENTIAL && !fileName.contains("-essential")) ||
-                            (gappsType == SettingsHelper.GAPPS_MICRO && !fileName.contains("-micro"))) {
+                    if ((gappsType == SettingsHelper.GAPPS_MINI && !fileName.contains("-mini"))
+                            ||
+                            (gappsType == SettingsHelper.GAPPS_STOCK && !fileName
+                                    .contains("-stock"))
+                            ||
+                            (gappsType == SettingsHelper.GAPPS_FULL && !fileName.contains("-full"))
+                            ||
+							(gappsType == SettingsHelper.GAPPS_ESSENTIAL && !fileName.contains("-essential"))
+                            ||
+                            (gappsType == SettingsHelper.GAPPS_MICRO && !fileName
+                                    .contains("-micro"))) {
                         list.remove(i);
                         i--;
                         continue;
@@ -233,7 +243,9 @@ public abstract class Updater implements Response.Listener<JSONObject>, Response
                 Utils.showToastOnUiThread(getContext(), getContext().getResources().getString(id)
                         + ": " + error);
             } else {
-                Utils.showToastOnUiThread(getContext(), id);
+                if (id != R.string.check_gapps_updates_error) {
+                    Utils.showToastOnUiThread(getContext(), id);
+                }
             }
         }
         mCurrentServer = -1;
